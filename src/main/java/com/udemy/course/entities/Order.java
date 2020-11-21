e8bf89a15b276b136fb84d1f09dd15e85992491d
Lucas Ferreira da Silva <lucferreira14@gmail.com>
2020-11-20 22:49:31 -0300

    Payment, one-to-one association

--- src/main/java/com/udemy/course/entities/Order.java
+++ src/main/java/com/udemy/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,6 +12,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -34,6 +36,9 @@ public class Order {
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 		// TODO Auto-generated constructor stub
@@ -69,6 +74,13 @@ public class Order {
 	public void setClient(User client) {
 		this.client = client;
 	}
+	public Payment getPayment() {
+		return payment;
+	}
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+	
 	public OrderStatus getOrderStatus() {
 		return OrderStatus.valueOf(orderStatus);
 	}
